c4d0a729c6435080168628e71bd71f3a204e83fd
Оляна Владимирова <olianavladimirova@gmail.com>
2025-03-10 16:05:22 +0300

    3соммше

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,7 @@ function game_status(game_status) {
 
 <template>
   <p>djfgidfhbkjfbkfb</p>
+  <p>slkdjhfdufjhgkdfgjdkfljblkgcbmgclk</p>
   <div class="buttons">
     <button @click="local_data.you_here = 1" :disabled="local_data.game_status"
       :class="{ you_here: local_data.you_here === 1 }">Настройки</button>
